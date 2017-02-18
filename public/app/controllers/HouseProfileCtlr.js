acad989d80df43457a4c7c3a218bac7dd177bb44
Guri Bergerud <gurikristine@gmail.com>
2017-02-18 19:22:41 +0100

    Added debugging console.log statement

--- public/app/controllers/HouseProfileCtlr.js
+++ public/app/controllers/HouseProfileCtlr.js
@@ -43,8 +43,8 @@ vidom.controller('HouseProfileCtlr', function ($scope, $rootScope, $sce, $routeP
 //2017-02-13 Guri: Was wondering if it would be better to send the file as an argument rather then finding it here..
         var file = document.getElementById('file').files[0];
 
-        console.log("Inside HouseProfileCtlr.savePicture. file is: " + file);
-        console.log("RouteParams.id is: " + $routeParams.id);
+//        console.log("Inside HouseProfileCtlr.savePicture. file is: " + file);
+//        console.log("RouteParams.id is: " + $routeParams.id);
 
         var img200x150URI = dataURItoBlob($scope.img200x150URI);
         var img400x300URI = dataURItoBlob($scope.img400x300URI);
