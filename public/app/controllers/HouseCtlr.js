9d81ace9fa1886b02c9142af3a89ad0c36724fdb
Guri Bergerud <gurikristine@gmail.com>
2017-02-21 19:41:44 +0100

    Took away hardcoding of s3 bucket

--- public/app/controllers/HouseCtlr.js
+++ public/app/controllers/HouseCtlr.js
@@ -3,8 +3,8 @@ vidom.controller('HouseCtlr', function ($scope, $rootScope, $location, HouseProf
     $scope.houses = $rootScope.user ? $rootScope.user.createdHouseProfile : [];
     angular.forEach($scope.houses, function(houseProfile){
         if (houseProfile.ref && houseProfile.ref.profilePicture){
-            console.log("\nHouseCtlr. s3Params.bucket is: "+ process.env.S3_BUCKET_NAME);
-            console.log("houseProfile.thumbnail url is: "+ "https://"+process.env.S3_BUCKET_NAME+"/"+houseProfile.ref.profilePicture.path);
+//            console.log("\nHouseCtlr. s3Params.bucket is: "+ process.env.S3_BUCKET_NAME);
+//            console.log("houseProfile.thumbnail url is: "+ "https://"+process.env.S3_BUCKET_NAME+"/"+houseProfile.ref.profilePicture.path);
 //            var text = "https://"+process.env.S3_BUCKET_NAME+"/"+houseProfile.ref.profilePicture.path;
 //            houseProfile.thumbnail = text;
 //            houseProfile.thumbnail = "https://vidomtestbucket.s3.amazonaws.com/"+houseProfile.ref.profilePicture.path;
