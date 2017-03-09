078772d94833f730da959b0e4d19384bc16c0756
Guri <gurikristine@gmail.com>
2017-03-09 07:46:17 +0100

    Working with creating large images

--- controllers/api/house.js
+++ controllers/api/house.js
@@ -52,11 +52,6 @@ router.post('/:profileId', function (req, res, next) {
                     uploaded: new Date(),
                     uploadedBy: req.body.userId
                 });
-                console.log("\nInside house.findOne. req.params.profileId is: "+req.params.profileId);
-                console.log("req.body.userId is: "+req.body.userId);
-                console.log("req.body.fileName is: "+req.body.fileName);
-                console.log("splitFileNames[0] is: "+splitFileNames[0]);
-                console.log("splitFileNames[1] is: "+splitFileNames[1]);
 /*2017-03-05 Guri: The profilePictureNumber (-1) and backgroundPictureNumber (-2) should be decleared as a 
  * global variable somewhere and used in the code in place of sending -1 and -2 directly.
  */
